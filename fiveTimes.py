2b66ce9afd3bf92c9f2775c2aa8ebcf7901a0b01
Stu Wares <stuwares@outlook.com>
2017-10-19 08:31:58 +0100

    added examples of while and for loops

---
+++ fiveTimes.py
@@ -0,0 +1,11 @@
+# intro to for loops
+print('My name is')
+for i in range(5):
+    print('Five times (' + str(i + 1) + ')')
+
+#as a while loop:
+print('My name is')
+i = 0
+while i < 5:
+    print('Five times (' + str(i + 1) + ')')
+    i = i + 1
